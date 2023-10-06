dfd777512d0272a1db709a77e438515c2d02f5ed
JohannCastillo <johannjco15022@gmail.com>
2023-10-06 15:05:06 -0500

    listado de rutas

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-# echo "Running composer"
-# composer global require hirak/prestissimo
-# composer install --working-dir=/var/www/html
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --working-dir=/var/www/html
 
-# echo "Running npm install"
-# npm install
+echo "Running npm install"
+npm install
 
-# echo "Running npm run dev"
-# npm run dev
+echo "Running npm run dev"
+npm run dev
 
-# echo "Caching config..."
-# php artisan config:cache
+echo "Caching config..."
+php artisan config:cache
 
-# echo "Caching routes..."
-# php artisan route:cache
+echo "Caching routes..."
+php artisan route:cache
 
-# echo "Running migrations..."
-# php artisan migrate:fresh --seed --force
+echo "Running migrations..."
+php artisan migrate:fresh --seed --force
 
 echo "Route listing..."
 php artisan route:list
