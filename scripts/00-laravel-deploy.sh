5037f15d828ebf458a0bfa4e89c22b7c50476955
JohannCastillo <johannjco15022@gmail.com>
2023-10-07 11:31:41 -0500

    check servidor

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,13 +3,19 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --working-dir=/var/www/html
 
+echo "*******************************************"
+
 echo "Running npm install"
 npm install
+
+echo "*******************************************"
 
 echo "Caching config..."
 php artisan route:clear
 php artisan config:clear
 php artisan config:cache
+
+echo "*******************************************"
 
 echo "Caching routes..."
 php artisan route:cache
@@ -17,11 +23,14 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate:fresh --seed --force
 
+echo "*******************************************"
 # echo "Route listing..."
 # php artisan route:list
 
 echo "Views ..."
 ls /var/www/html/resources/views
+
+echo "*******************************************"
 
 echo "Login ..."
 ls /var/www/html/resources/views/auth
